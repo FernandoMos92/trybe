9ccef5405f40547f95f85da120164af3b6a194ca
FernandoMos <fernandomosp@gmail.com>
2021-08-25 19:17:56 -0300

    complet first exercise in JS.

---
+++ Fundamentos/Bloco 3/4.1/script.js
@@ -0,0 +1,14 @@
+const myName = "Fernando";
+const bithCity = "São Paulo";
+let birthYear = 1992;
+
+console.log(`Meu nome é ${myName}, eu nasci em ${bithCity} no ano de ${birthYear}.`)
+
+birthYear = 2030;
+console.log(birthYear);
+
+/**
+ * Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+ * 
+ * R: Não se pode reatribuir o valor de uma constante. 
+ */
