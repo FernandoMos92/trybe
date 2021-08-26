433325f535349b78ccbb8596709448c37f1b9ce6
FernandoMos <fernandomosp@gmail.com>
2021-08-26 09:35:43 -0300

    Fourth task completed

--- Fundamentos/Bloco 3/4.1/script.js
+++ Fundamentos/Bloco 3/4.1/script.js
@@ -13,6 +13,8 @@
 const a = 15;
 const b = 35;
 const c = 20;
+const d = -10;
+const e = 0;
 
 let resultAdicao = a + b;
 console.log(resultAdicao);
@@ -54,3 +56,21 @@ if(a > b && a > c){
 /**
  *4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
  */
+
+ if(d < 0){
+  console.log("negative");
+ }
+ if(e === 0){
+   console.log("zero");
+ }
+ if(b > 0){
+   console.log("positive")
+ }
+
+ /**
+  * Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+  * 
+  * Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+  * 
+  * Um ângulo será considerado inválido se não tiver um valor positivo.
+  */
