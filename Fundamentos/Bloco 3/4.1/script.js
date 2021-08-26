2c88b5b9e1eefa23d1ba5810993a46108b332d21
FernandoMos <fernandomosp@gmail.com>
2021-08-26 09:28:39 -0300

    Third task completed

--- Fundamentos/Bloco 3/4.1/script.js
+++ Fundamentos/Bloco 3/4.1/script.js
@@ -12,6 +12,8 @@
 
 const a = 15;
 const b = 35;
+const c = 20;
+
 let resultAdicao = a + b;
 console.log(resultAdicao);
 
@@ -28,7 +30,7 @@ let resultModulo = (a % b);
 console.log(resultModulo);
 
 /**
- * Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+ * 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
  */
 
 if(a > b ){
@@ -38,5 +40,17 @@ if(a > b ){
 }
 
 /**
- * Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+ * 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+ */
+
+if(a > b && a > c){
+  console.log(`O numero ${a} é maior que todos os numeros.`);
+}else if(b > a && b > c){
+  console.log(`O numero ${b} é maior que todos os numeros.`);
+}else{
+  console.log(`O numero ${c} é maior que todos os numeros.`);
+}
+
+/**
+ *4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
  */
