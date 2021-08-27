b6bd005f9dfd1f80a9d8036232fd48d13043765a
FernandoMos <fernandomosp@gmail.com>
2021-08-27 12:46:02 -0300

    completed task 8

--- Fundamentos/Bloco 3/4.2/task8.js
+++ Fundamentos/Bloco 3/4.2/task8.js
@@ -3,3 +3,11 @@
  *8. Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
  * 
  */
+ let counter = [];
+ for (let index = 1; index <= 25; index++) {
+  counter.push(index);
+ } 
+console.log(counter);
+
+
+
