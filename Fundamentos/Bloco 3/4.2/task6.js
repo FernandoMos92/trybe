a5ce344f61d2535899c2c8312ee2605bac0e3f51
FernandoMos <fernandomosp@gmail.com>
2021-08-27 12:30:13 -0300

    completed task 6

--- Fundamentos/Bloco 3/4.2/task6.js
+++ Fundamentos/Bloco 3/4.2/task6.js
@@ -3,3 +3,13 @@
  *6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
  * 
  */
+
+ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+ for(let index = 0; index < numbers.length; index++ ){
+  if(numbers[index] % 2 === 1){
+    console.log(index + ":" + numbers[index]);
+  }else{
+    console.log("Nenhum valor ímpar encontrado");
+  }
+ }
