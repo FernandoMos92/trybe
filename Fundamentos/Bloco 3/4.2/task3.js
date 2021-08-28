fa7918a0471291920dca32274f5c2b3a7e17db71
FernandoMos <fernandomosp@gmail.com>
2021-08-27 23:15:01 -0300

    completed all questions

--- Fundamentos/Bloco 3/4.2/task3.js
+++ Fundamentos/Bloco 3/4.2/task3.js
@@ -5,10 +5,15 @@
  */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
-
+let container = 0;
 for (let index = 0; index < numbers.length; index++){
-    result = (numbers[index] + numbers[index+1]) / index
+   container += numbers[index];
 }
 
-console.log(result);
+console.log(container / numbers.length);
+
+// let result = 0;
+// numbers.map((el, index) =>{
+//     result = container += el/numbers.length;
+//     console.log(result.toFixed(2))
+// })
