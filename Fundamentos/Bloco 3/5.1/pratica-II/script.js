6126dc0b19b75b56d0c344bab6eb972e5ac726bd
FernandoMos92 <fernandomosp@gmail.com>
2021-09-09 13:21:15 -0300

    Completed second pratice

---
+++ Fundamentos/Bloco 3/5.1/pratica-II/script.js
@@ -0,0 +1,14 @@
+let containerH = document.querySelectorAll('#header-container');
+containerH[0].style.color = "#FFF";
+containerH[0].style.backgroundColor = "#10b069";
+
+
+let urgentes = document.querySelectorAll('.emergency-tasks');
+urgentes[0].style.backgroundColor = '#fc9f84';
+
+let noUrgentes = document.querySelectorAll('.no-emergency-tasks');
+noUrgentes[0].style.backgroundColor = '#f9db5e';
+
+let footerColor = document.querySelectorAll('#footer-container');
+footerColor[0].style.color = '#FFF'
+footerColor[0].style.backgroundColor = '#043533';
