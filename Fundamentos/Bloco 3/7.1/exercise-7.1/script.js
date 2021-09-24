07a4963a770de1b9266e24a57fe1797dbdaa4fc6
FernandoMos92 <fernandomosp@gmail.com>
2021-09-23 23:43:12 -0300

    Completed part one

--- Fundamentos/Bloco 3/7.1/exercise-7.1/script.js
+++ Fundamentos/Bloco 3/7.1/exercise-7.1/script.js
@@ -1,3 +1,10 @@
+/**
+ *1 - Agora você vai fazer alguns exercícios de fixação.
+  Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+  Modifique a estrutura da função para que ela seja uma arrow function .
+  Modifique as concatenações para template literals .
+ */
+
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,3 +17,14 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+/**
+ * 2 - Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+ */
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => a -b);
+console.log(oddsAndEvens)
