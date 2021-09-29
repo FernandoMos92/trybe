f7dc52c60b1defc360ec99bcd8dca6cf1907a458
FernandoMos92 <fernandomosp@gmail.com>
2021-09-29 12:02:54 -0300

    second part completed

--- Fundamentos/Bloco 3/7.3/script.js
+++ Fundamentos/Bloco 3/7.3/script.js
@@ -1,10 +1,18 @@
 const sum = (a, b) => {
-  if(typeof a === 'string' || typeof b === 'string') {
-    throw new Error('parameters must be numbers');
+  if (typeof a === "string" || typeof b === "string") {
+    throw new Error("parameters must be numbers");
   }
-  
- return a + b
+
+  return a + b;
 };
 
+const myRemove = (arr, item) => {
+  if (!arr.includes(item)) {
+    throw new Error("item not contains");
+  }
+  const findIndex = arr.indexOf(item);
+  arr.splice(findIndex, 1);
+  return `[${arr.toString()}]`;
+};
 
-module.exports = sum;
+module.exports = { sum, myRemove };
--- Fundamentos/Bloco 3/7.3/script.test.js
+++ Fundamentos/Bloco 3/7.3/script.test.js
@@ -1,4 +1,4 @@
-const sum = require('./script.js');
+const {sum, myRemove} = require('./script.js');
 
 describe('Verifica se está somando', ()=>{
   it("Teste se o retorno de sum(4, 5) é 9", ()=>{
@@ -10,6 +10,20 @@ describe('Verifica se está somando', ()=>{
   it(`Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)`, ()=>{
     expect(()=>{
       sum(4, '5');
-    }).toThrow('parameters must be numbers');
+    }).toThrow(Error);
+  })
+});
+
+describe("Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado", ()=>{
+  it("Válidando a saida", ()=>{
+    expect(myRemove([1, 2, 3, 4], 3)).toBe('[1,2,4]');
+  })
+  it("Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]", ()=>{
+    expect(myRemove([1, 2, 3, 4], 3)).not.toContain('[1, 2, 3, 4]');
+  })
+  it("Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado", ()=>{
+    expect(()=>{
+      myRemove([1, 2, 3, 4], 5);
+    }).toThrow(Error);
   })
 });
