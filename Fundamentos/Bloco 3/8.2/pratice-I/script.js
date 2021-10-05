42683ea676b333716bc8d63cdcacf08da094f241
FernandoMos92 <fernandomosp@gmail.com>
2021-10-05 15:25:58 -0300

    first pratice completed to some

--- Fundamentos/Bloco 3/8.2/pratice-I/script.js
+++ Fundamentos/Bloco 3/8.2/pratice-I/script.js
@@ -68,3 +68,15 @@ emailListInData.forEach(showEmailList);
   }
   
   console.log(findMusic('31031685'))
+
+  /**
+   * 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+   */
+
+   const names2 = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+   const hasName = (arr, name) => {
+      return arr.some(el => el === name)
+   }
+   
+   console.log(hasName(names2, 'Anaa')) 
