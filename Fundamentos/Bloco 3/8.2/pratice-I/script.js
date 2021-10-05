45ae2fd5813b391deb2bad1040c8b300bd4d801a
FernandoMos92 <fernandomosp@gmail.com>
2021-10-05 14:53:39 -0300

    completed tirty pratice

--- Fundamentos/Bloco 3/8.2/pratice-I/script.js
+++ Fundamentos/Bloco 3/8.2/pratice-I/script.js
@@ -21,6 +21,8 @@ emailListInData.forEach(showEmailList);
 /**
  * 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
  */
+
+//find
  const numbers = [19, 21, 31, 3, 46, 22, 16];
 
  const findDivisibleBy3And5 = (arr) => {
@@ -51,3 +53,18 @@ emailListInData.forEach(showEmailList);
  }
  
  console.log(findNameWithFiveLetters(names));
+
+ /**
+  * 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+  */
+  const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    return musicas.find(el => el.id === id);
+  }
+  
+  console.log(findMusic('31031685'))
