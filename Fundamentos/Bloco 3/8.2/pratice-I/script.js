b5adb75f1cf1d8d69b655335b562a251660dac68
FernandoMos92 <fernandomosp@gmail.com>
2021-10-05 15:55:33 -0300

    completed all pratice to claa 8.2

--- Fundamentos/Bloco 3/8.2/pratice-I/script.js
+++ Fundamentos/Bloco 3/8.2/pratice-I/script.js
@@ -99,3 +99,17 @@ emailListInData.forEach(showEmailList);
     }
     
     console.log(verifyAges(people, 18));
+
+    /**
+     * 1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente .
+     */
+     const people = [
+      { name: 'Mateus', age: 18 },
+      { name: 'José', age: 16 },
+      { name: 'Ana', age: 23 },
+      { name: 'Cláudia', age: 20 },
+      { name: 'Bruna', age: 19 },
+    ];
+
+    console.log(people.sort((peopleA, peopleB) => peopleA.age - peopleB.age));
+    console.log(people.sort((peopleA, peopleB) => peopleB.age - peopleA.age));
