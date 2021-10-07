17d178758c1ad94df41c816eb78da543f84a71c2
FernandoMos92 <fernandomosp@gmail.com>
2021-10-07 13:38:20 -0300

    FEATURE: Fifth exercise completed using reduce, split and for of

--- Fundamentos/Bloco-08/8.4/exercise5.js
+++ Fundamentos/Bloco-08/8.4/exercise5.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 // /5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 
 const names = [
@@ -6,6 +8,27 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
-function containsA() {
-  // escreva seu código aqui
+const expected = {
+  A:9,
+  a:11,
 }
+
+function containsA() {
+  const amountLetters = names.reduce((intialValue, currentValue) => {
+    let splitWords = currentValue.split('');
+    for (const item of splitWords) {
+      if(item === 'A') intialValue.A += 1;
+      if(item === 'a') intialValue.a += 1;
+    }
+    return intialValue;
+  }, {
+    A:0,
+    a:0,
+  })
+
+  return amountLetters;
+}
+
+containsA();
+
+assert.deepStrictEqual(containsA(), expected);
