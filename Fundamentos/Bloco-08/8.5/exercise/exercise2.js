9c8e192a93630d4a4c8d6a3b5e00c025874f8dc5
FernandoMos92 <fernandomosp@gmail.com>
2021-10-12 13:47:05 -0300

    Second task completed

--- Fundamentos/Bloco-08/8.5/exercise/exercise2.js
+++ Fundamentos/Bloco-08/8.5/exercise/exercise2.js
@@ -1,4 +1,4 @@
 // 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 // Dica: use parâmetro rest .
-
-// escreva sum abaixo
+const sum = (...rest) => rest.reduce((sum,sumb) => sum + sumb);
+console.log(sum(2, 4, 6, 8));
