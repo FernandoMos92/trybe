6c7a7a4b3469639800db485c49509bb07ba644a8
FernandoMos92 <fernandomosp@gmail.com>
2021-10-13 09:32:20 -0300

    Sixth exercise completed

--- Fundamentos/Bloco-08/8.5/exercise/exercise6.js
+++ Fundamentos/Bloco-08/8.5/exercise/exercise6.js
@@ -1,4 +1,4 @@
-// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Crie uma função toObject que, dada uma lista, retorna um objeto representando o carro:
 // Dica: use array destructuring e abbreviation object literal .
 
 const palio = ['Palio', 'Fiat', 2019];
@@ -6,3 +6,10 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+
+const toObject = ([model, brand, year]) => {
+const vehicle = {model, brand, year};
+return vehicle;
+};
+
+console.log(toObject(palio));
