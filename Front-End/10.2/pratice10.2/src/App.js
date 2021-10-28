1efae01d05c96ccf877d3459e856a38186560bd4
FernandoMos92 <fernandomosp@gmail.com>
2021-10-28 15:14:51 -0300

    first pratice completed

---
+++ Front-End/10.2/pratice10.2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Image from './Components/Image';
+import './cat.jpg';
+
+function App() {
+  return (
+    <>
+    <p>Test</p>
+    <Image source={"https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345__480.jpg"} alternativeText='Cute cat staring '/>
+    
+    </>
+  );
+}
+
+export default App;
