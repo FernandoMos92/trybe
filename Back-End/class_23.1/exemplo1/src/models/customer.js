8778fd6103f42af93910cf5bfae23872858cb38b
FernandoMos92 <fernandomosp@gmail.com>
2022-03-31 15:44:32 -0300

    feat: create mysql connection

--- Back-End/class_23.1/exemplo1/src/models/customer.js
+++ Back-End/class_23.1/exemplo1/src/models/customer.js
@@ -1,12 +1,4 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'model_database',
-  password: '251214',
-  port: 3306
-});
+const connection = require('./mysql-connection');
 
 const getAll = async () => {
   const query = 'SELECT * FROM model_database.customer'
